bf1bb463b841aecf4a44c1ebee9a16825823e862
fluent93 <fluent93@gmail.com>
2015-04-05 09:53:40 +0900

    no message

---
+++ Week 6/quiz6a_question7.py
@@ -0,0 +1,73 @@
+class BankAccount:
+    def __init__(self, initial_balance):
+        """Creates an account with the given balance."""
+        self.init_bal = initial_balance
+        self.fee = 0
+        
+    def deposit(self, amount):
+        """Deposits the amount into the account."""
+        self.init_bal += amount
+        
+    def withdraw(self, amount):
+        """
+        Withdraws the amount from the account.  Each withdrawal resulting in a
+        negative balance also deducts a penalty fee of 5 dollars from the balance.
+        """
+        if self.init_bal - amount < 0:
+            self.fee += 5
+            self.init_bal -= 5
+        self.init_bal -= amount
+        
+    def get_balance(self):
+        """Returns the current balance in the account."""
+        return self.init_bal
+    
+    def get_fees(self):
+        """Returns the total fees ever deducted from the account."""
+        return self.fee
+    
+    
+my_account = BankAccount(10)
+my_account.withdraw(5)
+my_account.deposit(10)
+my_account.withdraw(5)
+my_account.withdraw(15)
+my_account.deposit(20)
+my_account.withdraw(5) 
+my_account.deposit(10)
+my_account.deposit(20)
+my_account.withdraw(15)
+my_account.deposit(30)
+my_account.withdraw(10)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(50) 
+my_account.deposit(30)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5) 
+my_account.deposit(20)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.deposit(30)
+my_account.withdraw(25) 
+my_account.withdraw(5)
+my_account.deposit(10)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(10) 
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.deposit(30)
+my_account.withdraw(25) 
+my_account.withdraw(10)
+my_account.deposit(20)
+my_account.deposit(10)
+my_account.withdraw(5) 
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5) 
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5) 
+print my_account.get_balance(), my_account.get_fees()
